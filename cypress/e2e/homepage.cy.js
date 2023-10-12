d469773f910149498899a3aa0fb0fed1ffecaba4
HDZIVANOVICM <126469904+HDZIVANOVICM@users.noreply.github.com>
2023-10-12 11:05:41 +0200

    Adding new W&C test file homepage.cy.js

---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,28 @@
+describe('Work & Co website test', () => {
+    it('Opens W&C site, verifies the page title and headline', () => {
+      cy.visit('https://work.co/')
+      cy.title().should('eq','Work & Co | Digital Product Agency')
+      cy.get('[data-test-id="header-title-text"]').should('have.text','We solve complex problems through design & technology')
+    })
+
+    it('Opens W&C site, clicks on the W&C logo and checks page URL', () => {
+      cy.visit('https://work.co/')
+      cy.get('[data-test-id="global-menu-btn"]').click()
+      cy.url().should('eq','https://work.co/grid')
+    })
+
+    it('Opens W&C site, clicks on the W&C logo, clicks on "Select Clients" and checks page URL', () => {
+      cy.visit('https://work.co/')
+      cy.get('[data-test-id="global-menu-btn"]').click()
+      cy.get('[data-test-id="grid-select-clients-link"]').click()
+      cy.url().should('eq','https://work.co/clients/')
+    })
+
+    it('Opens W&C site, clicks on the W&C logo, clicks on "Expertise & Capabilities" and checks page URL', () => {
+      cy.visit('https://work.co/')
+      cy.get('[data-test-id="global-menu-btn"]').click()
+      cy.get('[data-test-id="grid-expertise-capabilities-link"]').click()
+      cy.url().should('eq','https://work.co/company/')
+    })
+
+  })
