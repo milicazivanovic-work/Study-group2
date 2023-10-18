eed358adbc3f62974e5b7513a6ae130898c2b17b
HDZIVANOVICM <126469904+HDZIVANOVICM@users.noreply.github.com>
2023-10-18 11:08:32 +0200

    Challenge 5 - Added fixture for login credentials, custom command for login and cart tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
