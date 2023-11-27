7dd4a3b1dc69ef087364bb149ca13b5fc6ab1e54
HDZIVANOVICM <126469904+HDZIVANOVICM@users.noreply.github.com>
2023-11-27 14:21:39 +0100

    Added the name for the screenshot

--- cypress/e2e/screenshot.cy.js
+++ cypress/e2e/screenshot.cy.js
@@ -1,12 +1,14 @@
+beforeEach(() => cy.visit("careers/offices/belgrade/"));
+
 describe("Take a screenshot", () => {
-  before(() => cy.visit("/"));
-
   it("should take a screenshot and store it in dedicated folder", () => {
-    cy.visit("careers/offices/belgrade/");
-
     //Just take a screenshot
-    cy.screenshot();
+    cy.screenshot("belgrade-office-screenshot", "screenshots/screenshot.cy.js");
+  });
+});
 
+describe("Take a screenshot", () => {
+  it("should compare screenshots using plugin", () => {
     //Compare screenshot using cypress-image-diff plugin
     cy.compareSnapshot("belgrade");
   });
